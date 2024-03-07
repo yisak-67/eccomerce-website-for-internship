a4efd9de03606bf7e020b778b366b654b55fec19
yisak-67 <yisakzemedu6774@gmail.com>
2024-03-07 19:47:01 +0300

    upload all filies

---
+++ admin/include/footer.php
@@ -0,0 +1,12 @@
+<!-- <style>
+	div.aaa{
+		background:black;
+	}
+</style>	
+<div class = "aaa">
+	<div class="footer">
+		<div class="container ">
+			
+				</div>
+	</div>
+</div> -->
---
+++ admin/include/header.php
@@ -0,0 +1,36 @@
+
+<div class="dc">
+    
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner bb">
+        <div class="container cc">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                <i class="icon-reorder shaded"></i>
+            </a>
+
+            <a class="brand" href="index.php">
+                Shopping Portal | Admin
+            </a>
+
+            <div class="nav-collapse collapse navbar-inverse-collapse">
+                <ul class="nav pull-right">
+                    <li><a href="#">
+                            Admin
+                        </a></li>
+                    <li class="nav-user dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="images/user.png" class="nav-avatar" />
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="change-password.php">Change Password</a></li>
+                            <li class="divider"></li>
+                            <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.nav-collapse -->
+        </div>
+    </div><!-- /navbar-inner -->
+</div><!-- /navbar -->
+</div>
